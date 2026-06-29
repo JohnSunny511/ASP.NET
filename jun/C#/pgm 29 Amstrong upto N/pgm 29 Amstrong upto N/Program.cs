02a5052c8876e60d1ef384c54a6b65da2baa034c
JohnSunny511 <johnsunnypalatty@gmail.com>
2026-06-29 12:02:50 +0530

    Amstrong upto N , 2 Pattern prinitings

---
+++ jun/C#/pgm 29 Amstrong upto N/pgm 29 Amstrong upto N/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgm_29_Amstrong_upto_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, rem, i,count;
+            Console.WriteLine("Enter the Limit");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i < n; i++)
+            {
+                int x = i, am = 0;
+                count = i.ToString().Length;
+                while (x > 0)
+                {
+                    rem = x % 10;
+                    am += Convert.ToInt32(Math.Pow(rem, count));
+                    x /= 10;
+                }
+                if(am == i)
+                {
+                    Console.WriteLine(am);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ jun/C#/pgm 30 Star Pattern 1/pgm 30 Star Pattern 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgm_30_Star_Pattern_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i,j;
+            Console.WriteLine("Enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i = n;i > 0; i --)
+            {
+                for(j = 0;j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine(" ");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
