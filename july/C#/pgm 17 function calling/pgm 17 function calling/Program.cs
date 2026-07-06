3f74cfe3bb8b1decb27b7ded2af6c6336ad1f8aa
JohnSunny511 <johnsunnypalatty@gmail.com>
2026-07-06 12:39:11 +0530

    Functions related programs

---
+++ july/C#/pgm 17 function calling/pgm 17 function calling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgm_17_function_calling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program o = new Program();
+
+            o.display();   // void function can be called by just creating a object of that class and use that to call the function
+
+            sfun();    //A static function can be called without creating an object
+             
+            int s = o.sum();         //An int Function can be called by assigning it to same data type variable
+            Console.WriteLine($"Sum: {s}");
+
+            o.product();   //When there is no return type just give all the instructions in the function itself
+
+            Console.ReadKey();
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Sample");
+        }
+       
+        public static void sfun()
+        {
+            Console.WriteLine("Static Function");
+        }
+
+        public int sum()
+        {
+            int x = 20, y = 30;
+            int s = x + y;
+            return s;
+        }
+
+        public void product()
+        {
+            int x = 20, y = 30;
+            int p = x * y;
+            Console.WriteLine($"{x} x {y} = {p}");
+        }
+    }
+}
---
+++ july/C#/pgm 18 Factorial using int function/pgm 18 Factorial using int function/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgm_18_Factorial_using_int_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program ob = new Program();
+            int res = ob.fact();
+            Console.WriteLine($"Factorial = {res}");
+            Console.ReadKey();
+        }
+
+        public int fact()
+        {
+            Console.WriteLine("Enter the number");
+            int n = Convert.ToInt32(Console.ReadLine()), res = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                res *= i;
+            }
+
+            return res;
+        }
+    }
+}
