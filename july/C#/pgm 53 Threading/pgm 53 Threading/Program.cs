13edae100d0d967d6ff3dac15b57ed285202590a
JohnSunny511 <johnsunnypalatty@gmail.com>
2026-07-18 20:41:39 +0530

    Threading and multithreading implementations

---
+++ july/C#/pgm 53 Threading/pgm 53 Threading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace pgm_53_Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread ob = new Thread(display);
+            ob.Start();
+            Console.ReadKey();
+        }
+
+        public static void display()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Thread is {0}", i);
+            }
+        }
+    }
+}
---
+++ july/C#/pgm 54 Multithreading/pgm 34 Multithreading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pgm_34_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
